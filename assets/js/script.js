a2c845613a6f04d5a082c9a054aaab5ee2df290e
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-01-30 20:25:09 +0000

    Add question and answers to questionBox variable

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// Question box
+
+let questionBox = [
+    {
+        question : "",
+        options : [", ", ", "]
+        answer : ""
+    },
+    {
+        question : "How many sides has a Hexagon?",
+        options : ["5", "6", "7", "8"]
+        answer : "6"
+    },
+    {
+        question : "What is the longest river in the world?",
+        options : ["Amazon River", "Nile", "Yellow River", "Congo River"],
+        answer : "Nile"
+    }, 
+    {
+        question : "In which country was the airline Ryanair founded?",
+        options : ["Germany", "Ireland", "Scotland", "Spain"],
+        answer : "Irland"
+    }, 
+    {
+        question : "Who was assassinated on the 22nd of November 1963?",
+        options :["Martin Luther King", "Jhon F Kennedy", "MK Gandhi", "Malcom X"],
+        answer : "Jhon F Kennedy"
+    }, 
+    {
+        question : "Which band released the song “Wonderwall” in the 90s?",
+        options : ["Oasis", "Joy Divison", "The Verge", "Nirvana"],
+        answer : "Oasis"
+    }, 
+    {
+        question : "What was Euclid?",
+        options : ["A philosopher", "A poet", "A painter", "A mathematician"],
+        answer : "A mathematician"
+    }, 
+    {
+        question : "What is the capital of Iraq?",
+        options :["Baghdad", "Islamabad", "Tehran", "Amman"],
+        answer :"Baghdad"
+    }, 
+    {
+        question : "What colour is the “m” from the McDonald’s logo?",
+        options :["Blue", "Red", "Yellow", "Black"],
+        answer :"Yellow"
+    }, 
+    {
+        question : "What is guacamole made of?",
+        options :["Banana", "Yoghurt", "Avocado", "Chick Pea"],
+        answer :"Avocado"
+    },
+]
