e25060b935301879e79ce8297022a27502fd3ddb
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-02-04 23:11:20 +0000

    Create new file for questions box and import it to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,3 @@
-// - Question box -
-
-let questionsBox = [
-    {
-        question : "How many bones are there in an adult human body?",
-        options : ["186", "206", "286", "306"],
-        answer : "206"
-    },
-    {
-        question : "How many sides has a Hexagon?",
-        options : ["5", "6", "7", "8"],
-        answer : "6"
-    },
-    {
-        question : "What is the longest river in the world?",
-        options : ["Amazon River", "Nile", "Yellow River", "Congo River"],
-        answer : "Nile"
-    }, 
-    {
-        question : "In which country was the airline Ryanair founded?",
-        options : ["Germany", "Ireland", "Scotland", "Spain"],
-        answer : "Irland"
-    }, 
-    {
-        question : "Who was assassinated on the 22nd of November 1963?",
-        options :["Martin Luther King", "Jhon F Kennedy", "MK Gandhi", "Malcom X"],
-        answer : "Jhon F Kennedy"
-    }, 
-    {
-        question : "Which band released the song “Wonderwall” in the 90s?",
-        options : ["Oasis", "Joy Divison", "The Verge", "Nirvana"],
-        answer : "Oasis"
-    }, 
-    {
-        question : "What was Euclid?",
-        options : ["A philosopher", "A poet", "A painter", "A mathematician"],
-        answer : "A mathematician"
-    }, 
-    {
-        question : "What is the capital of Iraq?",
-        options :["Baghdad", "Islamabad", "Tehran", "Amman"],
-        answer :"Baghdad"
-    }, 
-    {
-        question : "What colour is the “m” from the McDonald's logo?",
-        options :["Blue", "Red", "Yellow", "Black"],
-        answer :"Yellow"
-    }, 
-    {
-        question : "What is guacamole made of?",
-        options :["Banana", "Yoghurt", "Avocado", "Chick Pea"],
-        answer :"Avocado"
-    }
-]
-
 
 // - Question container variables -
 
@@ -76,6 +21,11 @@ function displayQuestion(questionsBox) {
 }
 displayQuestion(questionsBox);
 
+// - Function to display next question -
+
+$(".option").click(function() {
+    next(randomQuestions);
+})
 
 
 
